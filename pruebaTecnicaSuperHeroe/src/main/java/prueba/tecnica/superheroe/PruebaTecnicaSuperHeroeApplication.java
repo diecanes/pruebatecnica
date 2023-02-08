949e6e342f53b368c5c491472b90eedea26969da
Diego Cantos <didaccantos@gmail.com>
2023-02-08 20:27:28 +0100

    Commit Init Project

---
+++ pruebaTecnicaSuperHeroe/src/main/java/prueba/tecnica/superheroe/PruebaTecnicaSuperHeroeApplication.java
@@ -0,0 +1,13 @@
+package prueba.tecnica.superheroe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSuperHeroeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaSuperHeroeApplication.class, args);
+	}
+
+}
---
+++ pruebaTecnicaSuperHeroe/src/test/java/prueba/tecnica/superheroe/PruebaTecnicaSuperHeroeApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.tecnica.superheroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSuperHeroeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
